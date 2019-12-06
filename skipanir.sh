d35a8c63dab576621f3f7612b7b05beaaee8336a
Bjarki Jóhannes Elfarsson <39884037+BjarkiJohannes@users.noreply.github.com>
2019-12-06 15:10:51 +0000

    Update skipanir.sh

--- skipanir.sh
+++ skipanir.sh
@@ -5,6 +5,148 @@ mkdir Leikdeild
 mkdir Songdeild
 mkdir Gjafadeild
 adduser (allar notendur)
-usermod -a -G- (deild við viðegjanda notendur) (usera við viðeigjandi deildar)
-cat e
-                                                                        
+sudo usermod -L -e 1 askur
+sudo usermod -L -e 1 askur 
+sudo usermod -L -e 1 skellir 
+passwd -e gryla 
+passwd -e staur 
+passwd -e gaur 
+passwd -e stufur 
+passwd -e thvari 
+passwd -e skefill 
+passwd -e askur 
+passwd -e skellir 
+passwd -e gamur 
+passwd -e glugg
+passwd -e gluggi 
+passwd -e gatti 
+passwd -e krokur 
+passwd -e kerti 
+usermod -L askur
+usermod -L skellir 
+cd /jolagogn/
+cat /etc/group
+gryla sudo
+  218  sudo gryla
+  219  whoami gryla
+  220  whoami --
+  221  whoami -- aG gryl
+  222  history
+  223  cd /gryla/skjal
+  224  cat /skajl
+  225  cd /etc/skjal
+  226  sudo -i
+  227  cd /jolagogn/
+  228  mkdir Sameign
+  229  mdkir Gjafadeild
+  230  mddir Gjafadeild
+  231  mkdir Gjafadeild
+  232  mkdir Leikdeild
+  233  mkdir Songdeild
+  234  groupadd gjafadeild
+  235  groupadd leikdeild
+  236  groupadd songdeild
+  237  groupadd allir
+  238  cat /etc/group
+  239  cd /etc/skel
+  240  ls
+  241  mkdir Vinna
+  242  mkdir Leikir
+  
+  243  ls
+  244  adduser leppur
+  245  cat /etc/passwd
+  246  cd /home/bjarki
+  247  ls
+  248  cd /home/leppur/
+  249  ls
+  250  cd /etc/skel
+  251  ln -s /jolagogn/ jolagogn
+  252  ls -l
+  253  cd jolagogn
+  254  ls
+  255  adduser gryla
+  256  cd /home/gryla/
+  257  ls
+  258  ls -l
+  259  cd jolagogn
+  260  ls 
+  261  adduser staur
+  262  adduser gaur
+  263  adduser stufur
+  264  adduser thvari
+  265  adduser skefill
+  266  adduser
+  267  adduser askur
+  268  adduser skellir
+  269  adduser gamur
+  270  adduser bjugi
+  271  adduser gluggi 
+  272  adduser gatti
+  273  userdel gatti
+  274  cd /etc/group
+  275  cd /etc/group/
+  276  cd /etc/groap
+  277  cd /etc/grup
+  278  cd /etc/group
+  279  cd /etc/group/
+  280  cd ..
+  281  ls
+  282  ls -i
+  283  cd /home/passwd
+  284  cd /home/passwd/
+  285  cd /etc/passwd
+  286  cd /etc/passwd/
+  287  cat /etc/group
+  288  adduser gatti
+  289  adduser krokur
+  290  adduser kerti
+  291  cat /etc/group
+  292  cd /jolagogn/
+  293  cd /etc/group
+  294  cd /etc/group/
+  295  cd /etc/passwd
+  296  cat /etc/group
+  297  usermod -a -G allir gryla 
+  298  cat /etc/group
+  299  usermod -a -G allir staur 
+  300  usermod -a -G allir gaur
+  301  usermod -a -G allir stufur
+  302  usermod -a -G allir thvari
+  303  usermod -a -G allir skefill
+  304  usermod -a -G allir askur
+  305  usermod -a -G allir skellir 
+  306  usermod -a -G allir gamur
+  307  usermod -a -G allir bjugi 
+  308  usermod -a -G allir gluggi 
+  309  usermod -a -G allir gattt
+usermod -a -G allir gatti 
+usermod -a -G allir krokur 
+usermod -a -G allir kerti 
+cat /etc/group
+usermod -a -G gjafadeild staur 
+usermod -a -G gjafadeild gaur 
+usermod -a -G gjafadeild gryla 
+usermod -a -G gjafadeild stufur 
+usermod -a -G gjafadeild thvari 
+usermod -aG sudo gryla
+su - gryla 
+usermod -a -G gjafadeild skefill
+usermod -a -G gjafadeild askur 
+usermod -a -G gjafadeild skellir 
+usermod -a -G leikdeild gamur 
+usermod -a -G 
+usermod -a -G songdeild gatti 
+usermod -a -G songdeild krokur 
+usermod -a -G songdeild kerti 
+usermod -a -G leikdeild gluggi 
+cd /home/gryla/
+ls
+cd ..
+cat /etc/passwd
+cat /etc/group
+usermod -a -G leikdeild gryla 
+usermod -a -G songdeild gryla 
+cat /etc/group
+sudo -i
+
